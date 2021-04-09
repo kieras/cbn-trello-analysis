6b27ecda1c7e5636d6284f06302560a029c236f6
felipegc <felipecastro07@gmail.com>
2021-04-09 13:17:25 -0300

    adding create_workspace script

--- utils/create_workspace.sh
+++ utils/create_workspace.sh
@@ -2,18 +2,34 @@
 
 # This script must be run from cloud-shell
 # It will generate the user's workspace by:
-#  - Installing the python dependecies
+#  - Removing the project folder if it exist
 #  - Fetching the project from git hub
+#  - Installing the python dependecies
 #  - Creating an alias to startup the jupyter
 
 VM_NAME=cbn-metrics
 ZONE=us-central1-a
-GIT_USER=****
-GIT_PASSWORD=****
+GIT_USER=$1
+GIT_PASSWORD=$2
 
-USER=$(whoami)
+gcloud compute ssh --zone $ZONE $VM_NAME --command " \
+    rm -rf cbn-trello-analysis/ && \
+    git clone -b metrics_sheet_trello https://${GIT_USER}:${GIT_PASSWORD}@github.com/kieras/cbn-trello-analysis.git && \
+    sh ./cbn-trello-analysis/utils/install_packages.sh &&\
+    sudo cp -v ../cbn-metrics-keys/keys.json ./cbn-trello-analysis && \
+    sudo cp -v ../cbn-metrics-keys/cbn-trello-metrics-sa.json ./cbn-trello-analysis \
+  "
 
-#sh install_packages.sh
-gcloud compute ssh --zone $ZONE $VM_NAME --command \
-    "git clone -b metrics_sheet_trello https://${GIT_USER}:${GIT_PASSWORD}@github.com/kieras/cbn-trello-analysis.git && \
-    touch file.txt"
+is_alias_available=$(cat .bashrc | grep cbn-metrics=)
+
+if [ -z "$is_alias_available" ]; then
+  echo "Creating the alias for cbn-metrics"
+  echo "" >> .bashrc
+  echo "alias cbn-metrics=\"gcloud compute ssh $VM_NAME \\
+       --zone $ZONE \\
+       -- -L 8080:localhost:8888 -4 \\
+       '/opt/cbn-metrics/bin/run_jupyterlab'\"" \
+   >> .bashrc
+else
+  echo "The cbn-metrics alias already exists. Nothing to do."
+fi
