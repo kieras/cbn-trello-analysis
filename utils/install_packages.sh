16343d8c0dd093e302fa255bf930cf76541682ad
Victor Figueiredo <vfigueiredo@ciandt.com>
2021-07-23 10:42:34 -0300

    Change packege gspread to official Google API

--- utils/install_packages.sh
+++ utils/install_packages.sh
@@ -2,7 +2,7 @@
 
 pip3 install pandas
 pip3 install numpy==1.18.3
-pip3 install gspread
 pip3 install oauth2client
 pip3 install matplotlib
 pip3 install python-dateutil
+pip3 install --upgrade google-api-python-client
