88b45b5318593a07c0906ce271a5919c7ac32664
Victor Figueiredo <vfigueiredo@ciandt.com>
2021-07-23 10:58:44 -0300

    Romove --upgrade from instal_packages.sh

--- utils/install_packages.sh
+++ utils/install_packages.sh
@@ -5,4 +5,4 @@ pip3 install numpy==1.18.3
 pip3 install oauth2client
 pip3 install matplotlib
 pip3 install python-dateutil
-pip3 install --upgrade google-api-python-client
+pip3 install google-api-python-client
